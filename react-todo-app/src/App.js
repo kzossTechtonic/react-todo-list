e8a2d3e52d74f8e76a608df49d734fba32871fb9
karenna <karenna.zoss@techtonic.com>
2019-02-27 20:52:58 -0700

    everything before routing

--- react-todo-app/src/App.js
+++ react-todo-app/src/App.js
@@ -1,37 +1,63 @@
 import React, { Component } from 'react';
 import './App.css';
 import Todos from './Components/Todos'
+import Header from './Components/Header'
+import AddTodo from './Components/AddTodo'
+import uuid from 'uuid';
 
 class App extends Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuid.v4(),
         title: 'Take out the trash',
         completed: false
       },
       {
-        id: 2,
+        id: uuid.v4(),
         title: 'Dinner with dude',
         completed: true 
       },
       {
-        id: 3,
+        id: uuid.v4(),
         title: 'Meeting with boss',
         completed: false
       } 
     ]
   }
 
+  //Toggle Complete
 markComplete = (id) => {
-  console.log(id)
+  this.setState({ todos: this.state.todos.map(todo => {
+    if(todo.id === id) {
+      todo.completed = !todo.completed
+    }
+    return todo;
+  }) });
+}
+
+delTodo= (id) => {
+  this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] });
+}
+
+addTodo = (title) => {
+  const newTodo = {
+    id: uuid.v4(),
+    title,
+    completed: false
+  }
+  this.setState({ todos: [...this.state.todos, newTodo] });
 }
 
   render() {
     console.log(this.state.todos)
     return (
       <div className="App">
-       <Todos todos={this.state.todos} markComplete={this.markComplete}/> 
+      <div className="container">
+        <Header />
+        <AddTodo addTodo={this.addTodo} />
+       <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/> 
+       </div>
       </div>
     );
   }
